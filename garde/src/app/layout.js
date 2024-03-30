1aa542295ed262dfc586aea78fb2014f44c9b828
Demon_Venerable <111742606+SujashB@users.noreply.github.com>
2024-03-30 15:31:13 -0700

    Changed file struct. Finished Hero Section

--- garde/src/app/layout.js
+++ garde/src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
 
+import "./globals.css";
+import Hero from "../components/Hero";
+import navbar from "../components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+          {/* <Navbar/> */}
+          <Hero />
+      {children}</body>
     </html>
   );
 }

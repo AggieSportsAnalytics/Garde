77d28041ff5195b640ec269436eef263d3e13db7
SujashB <sjbarman@ucdavis.edu>
2024-05-08 03:46:29 -0700

    Added the UI for Coach Sign in and Sign Up

--- garde/src/app/sign-in/[[...sign-in]]/page.jsx
+++ garde/src/app/sign-in/[[...sign-in]]/page.jsx
@@ -5,6 +5,11 @@ export default function Page() {
   return (
   <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <SignIn />
+      <h2 className="mt-2 text-white flex justify-center">Coaches please enter your access key below.</h2>
+      <div className="flex justify-center mt-5">
+        <input type="password" className="rounded-md"></input>
+        <input type="button" className="ml-5 text-white rounded-lg bg-slate-800 p-2" value="Submit" />
+      </div>
   </div>
   );
 

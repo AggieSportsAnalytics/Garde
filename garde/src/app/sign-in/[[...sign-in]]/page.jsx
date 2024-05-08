00694ec0b02f14f5ad7bda0a6d6af6ff45b51a90
SujashB <sjbarman@ucdavis.edu>
2024-05-08 03:50:45 -0700

    Implemented Coach Page Sign in

--- garde/src/app/sign-in/[[...sign-in]]/page.jsx
+++ garde/src/app/sign-in/[[...sign-in]]/page.jsx
@@ -1,4 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
+import Link from "next/link";
 import "../../../app/globals.css"
 
 export default function Page() {
@@ -8,7 +9,9 @@ export default function Page() {
       <h2 className="mt-2 text-white flex justify-center">Coaches please enter your access key below.</h2>
       <div className="flex justify-center mt-5">
         <input type="password" className="rounded-md"></input>
-        <input type="button" className="ml-5 text-white rounded-lg bg-slate-800 p-2" value="Submit" />
+        <Link href="/coach_page">
+          <input type="button" className="ml-5 text-white rounded-lg bg-slate-800 p-2" value="Submit" />
+        </Link>
       </div>
   </div>
   );

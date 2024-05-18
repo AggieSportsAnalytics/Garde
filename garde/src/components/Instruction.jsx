b1c3b409c2d8c1091fd7e71dc15f6acd361016e9
Vishal Koppuru <vishalksai@gmail.com>
2024-05-17 18:43:20 -0700

    Initial timer-instruction linking

--- garde/src/components/Instruction.jsx
+++ garde/src/components/Instruction.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Instruction = () => {
+const Instruction = ({ instructionIndex, instructions }) => {
+  let instruction;
+
+  if (instructionIndex === -1) {
+    instruction = 'Awaiting instruction';
+  } else if (instructionIndex >= instructions.length) {
+    instruction = 'End of instructions';
+  } else {
+    instruction = instructions[instructionIndex];
+  }
   return (
     <div className="border border-gray-300 rounded p-2 bg-white w-1/4 " style={{ maxHeight: '10vh', margin: '1vh auto', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '1.5rem',backgroundColor: '#2D2D2D', color: '#FFF' }}>
-      Perform en garde
+      {instruction}
     </div>
   );
 };

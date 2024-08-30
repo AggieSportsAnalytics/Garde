0b11e3648d18ee131ff0088f2b8a49c1d8ce5bdd
Vikram Penumarti <vikram.penumarti@gmail.com>
2024-08-29 23:08:37 -0700

    frontend

--- garde/src/app/page.js
+++ garde/src/app/page.js
@@ -1,6 +1,5 @@
-"use client"
+"use client";
 
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Features from "../components/Features";
@@ -8,29 +7,17 @@ import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Showcase from "../components/Showcase";
 import Banner from "../components/Banner";
-import Fencer_Page from "./fencer_page/page";
-import CoachPage from "./coach_page/page";
 
 export default function Home() {
-     
-        return (
-        
-            <div className="">
-                {/* <Navbar /> */}
-                <Hero />
-                <About />
-                <Showcase />
-                <Features />
-                <Banner />
-                <Contact />
-                <Footer />
-            </div>
-        )
-     
-   //   return ( 
-   //      <div className="">
-   //          <Fencer_Page />
-            
-   //       </div>
-   //   )
+	return (
+		<div className="">
+			<Hero />
+			<About />
+			<Showcase />
+			<Features />
+			<Banner />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
--- garde/src/components/InstructionContext.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'; 
-
-const InstructionContext = React.createContext(); 
-
-export default InstructionContext;

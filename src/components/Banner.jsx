4aeef6acdf7ac6299bf050f8cffdf7010046f7a7
Vikram Penumarti <vikram.penumarti@gmail.com>
2024-08-30 13:25:22 -0700

    light dark mode

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,12 +3,12 @@ import { useTheme } from "next-themes"; // Assuming you are using next-themes fo
 import { MagicCard } from "./ui/MagicCard"; // Import MagicCard component
 
 const Banner = () => {
-	const { theme } = useTheme();
+	// const { theme } = useTheme();
 	return (
 		<div className="mt-16 px-4">
 			<MagicCard
 				className="cursor-pointer justify-center shadow-2xl text-center text-4xl sm:text-5xl md:text-6xl h-[150px] sm:h-[180px] md:h-[200px] whitespace-normal"
-				gradientColor={theme === "dark" ? "#262626" : "#D9D9D955"}
+				// gradientColor={theme === "dark" ? "#262626" : "#D9D9D955"}
 			>
 				<div className="flex items-center justify-center h-full">
 					<h1 className="font-platypi font-extrabold text-white">

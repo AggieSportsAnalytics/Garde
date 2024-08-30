909df028bfa10a269faefcedf1d8425ef8634b4e
Vikram Penumarti <vikram.penumarti@gmail.com>
2024-08-30 00:16:58 -0700

    finishing touches

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 export default {
-    images: {
-        domains: ['images.unsplash.com', 'assets.aceternity.com', 'unsplash.com']
-    }
+	images: {
+		domains: ["images.unsplash.com", "assets.aceternity.com", "unsplash.com"],
+		loader: "default",
+		unoptimized: true,
+	},
+	output: "export",
 };

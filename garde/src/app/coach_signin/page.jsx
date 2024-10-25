9b57282c2d9cb878f41d12545ad649324032bf32
Rishit Das <rdas@ucdavis.edu>
2024-10-25 13:17:20 -0700

    Attempt at fixing login

--- garde/src/app/coach_signin/page.jsx
+++ garde/src/app/coach_signin/page.jsx
@@ -1,6 +1,8 @@
 import React, { Suspense } from 'react';
 import Signin from "../../components/auth/Signin";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+
 export default function () {
 	return (
 		<Suspense fallback={<div>Loading...</div>}>

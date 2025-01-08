f769c09d9b9e0092eb46aecd1fc0eccb5c7c6f97
Vikram Penumarti <vikram.penumarti@gmail.com>
2025-01-07 18:57:45 -0800

    dynamic generation of renew_session

--- garde/src/app/api/renew_session/route.js
+++ garde/src/app/api/renew_session/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
+export const dynamic = "force-dynamic";
+
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export async function GET(req) {

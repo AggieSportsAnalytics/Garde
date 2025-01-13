3109b44e2a3d7e15dd2acbe4f0c425d515b71435
Vikram Penumarti <vikram.penumarti@gmail.com>
2025-01-13 14:38:52 -0800

    privacy policy

---
+++ garde/src/app/privacy-policy/page.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+const PrivacyPolicy = () => {
+	const [htmlContent, setHtmlContent] = useState("");
+
+	useEffect(() => {
+		fetch("/privacy-policy.html")
+			.then((response) => response.text())
+			.then((data) => setHtmlContent(data))
+			.catch((error) => console.error("Error loading the HTML file:", error));
+	}, []);
+
+	return (
+		<div className="privacy-policy bg-white">
+			<div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+		</div>
+	);
+};
+
+export default PrivacyPolicy;

1c9e595a1c10ce5b117eaad4af57efb090df24cd
Vikram Penumarti <vikram.penumarti@gmail.com>
2025-02-24 12:03:52 -0600

    changing deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# 1. pulling latest
+# 2. installing frontend deps
+# 3. building frontend
+# 4. installing worker deps
+# 5. deploying worker
+# 6. stopping current prod server
+# 7. restarting/starting prod server
+
 set -e # Exit immediately if any command fails
 
 # Navigate to the directory containing this script
 cd "$(dirname "$0")"
-
-# Stop the PM2 process if it exists
-pm2 stop garde-prod || echo "garde-prod is not running, skipping stop."
 
 # Navigate to the project directory
 echo "Changing to the project directory..."
@@ -37,6 +42,13 @@ fi
 echo "Changing to the Cloudflare worker directory..."
 cd ../cloud-setup/garde
 
+# Install dependencies
+echo "Installing dependencies..."
+if ! npm install; then
+    echo "npm install failed. Exiting."
+    exit 1
+fi
+
 # Deploy Cloudflare worker
 echo "Deploying Cloudflare worker..."
 if ! wrangler deploy; then
@@ -46,6 +58,9 @@ fi
 
 # Navigate back to the project directory for PM2 commands
 cd ../../garde
+
+# Stop the PM2 process if it exists
+pm2 stop garde-prod || echo "garde-prod is not running, skipping stop."
 
 # Start or restart the PM2 process
 if pm2 describe garde-prod > /dev/null; then

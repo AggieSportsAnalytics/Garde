a9f107c4fc3ff1e847327e8b095eca6d6712fc52
Vikram Penumarti <vikram.penumarti@gmail.com>
2025-04-07 21:58:36 -0700

    oauth iOS Google client id

--- garde/capacitor.config.ts
+++ garde/capacitor.config.ts
@@ -1,17 +1,17 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.gardeai.app',
-  appName: 'Garde',
-  // webDir: 'dist',
+  appId: "com.gardeai.app",
+  appName: "Garde",
   server: {
-    url: 'https://gardeai.com',
+    url: process.env.NEXT_PUBLIC_BASE_URL,
     cleartext: true,
   },
   plugins: {
     GoogleAuth: {
       scopes: ["profile", "email", "openid"],
-      serverClientId: process.env.NEXT_PUBLIC_IOS_GOOGLE_CLIENT_ID,
+      iosClientId: process.env.NEXT_PUBLIC_IOS_GOOGLE_CLIENT_ID,
+      clientId: process.env.NEXT_PUBLIC_IOS_GOOGLE_CLIENT_ID,
       forceCodeForRefreshToken: true
     }
   }
